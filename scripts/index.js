5e880fa67cecd1262fd1eb95cb6de8b8158e756d
Yohan_Garcia <75887329+yohanJS@users.noreply.github.com>
2021-08-24 17:53:54 -0400

    added a toggle icon

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,22 @@
 "use strict"
+
+const $ = (id) => {
+    return document.getElementById(id);
+}
+
+const changeToggle = () => {
+    let toggle = $("toggle");
+    if(toggle.getAttribute("src") == "./images-logo/toogle.svg") {
+        toggle.src = "./images-logo/toogle-off.svg";
+    } 
+    else if (toggle.getAttribute("src") == "./images-logo/toogle-off.svg") {
+        toggle.src = "./images-logo/toogle.svg";
+    }
+}
+
+
+window.onload = () => {
+    $("toggle").onclick = () => {
+        changeToggle();
+    }
+}
